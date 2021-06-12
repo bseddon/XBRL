5b894de3b230a39f592b885a629f08c633e27777
bseddon <bill.seddon@lyquidity.com>
2021-06-12 22:11:43 +0000

    Compute package hashes save into signed Xml doc

--- examples/NLSBR/package_hashes.php
+++ examples/NLSBR/package_hashes.php
@@ -7,50 +7,89 @@
  * 2021-06-11
  */
 
-$zip = new ZipArchive();
-if ( $zip->open( '... link to package zip file ...' ) !== true )
+require __DIR__ . '/../../XBRL.php';
+
+$zip = new \ZipArchive();
+if ( $zip->open( '... path to package zip ...' ) !== true )
 	return;
 
 $hashes = array();
 
-// Read the files and folders
-for ( $index = 0; $index < $zip->numFiles; $index++ )
+try
 {
-	$name = $zip->getNameIndex( $index );
-	if ( $name[ strlen( $name  ) -1 ] == '/' ) continue;
-
-	$file = $zip->getFromIndex( $index );
-
-	$ext = strtoupper( pathinfo( $name, PATHINFO_EXTENSION ) );
-
-	switch( $ext )
+	// Read the files and folders
+	for ( $index = 0; $index < $zip->numFiles; $index++ )
 	{
-		case "XML":
-		case "XBRL":
-		case "XSD":
-		case "HTML":
-		case "XHTML": //These files can be canonicalized
+		// Skip folders
+		$name = $zip->getNameIndex( $index );
+		if ( $name[ strlen( $name  ) -1 ] == '/' ) continue;
+
+		$file = $zip->getFromIndex( $index );
+		$ext = strtoupper( pathinfo( $name, PATHINFO_EXTENSION ) );
+
+		switch( $ext )
 		{
-			$dom = new DOMDocument();
-			$dom->loadXML( $file );
-			$file = $dom->C14N( false, true );
-			$dom = null;
-			unset( $dom );
+			case "XML":
+			case "XBRL":
+			case "XSD":
+			case "HTML":
+			case "XHTML": //These files can be canonicalized
+			{
+				$dom = new DOMDocument();
+				$dom->loadXML( $file );
+				// Normalize the XML content
+				$file = $dom->C14N( false, true );
+				$dom = null;
+				unset( $dom );
 
-			break;
+				break;
+			}
 		}
-	}
 
-	$hashes[ $name ] = substr( base64_encode( hash( 'sha256', $file, true ) ), -16 );
+		$hashes[ $name ] = substr( base64_encode( hash( 'sha256', $file, true ) ), -16 );
+	}
+}
+catch( \Exception $ex )
+{
+	echo "Oops. " . $ex->getMessage();
+	return;
+}
+finally
+{
+	$zip->close();
 }
 
-$zip->close();
-
-// Case insensitive sort
+// Case insensitive 
 asort( $hashes, SORT_FLAG_CASE|SORT_STRING );
 
-// Add the total hash to the list
+// Compute the overall hash
 $hashes['totalHash'] = substr( base64_encode( hash( 'sha256', join( '', array_values( $hashes ) ), true ) ), -16 );
 
-// Save as a JSON file
-file_put_contents( __DIR__ . '/sbr_hashes.json', json_encode( $hashes, JSON_PRETTY_PRINT ) );
+// Add a hash element to a parent
+$addHash = function( \DOMElement $root, string $file, string $value ) 
+{
+	$hash = $root->ownerDocument->createElement('hash');
+	$root->appendChild( $hash );
+	$hash->setAttribute( 'file', $file );
+	$hash->setAttribute( 'value', $value );
+};
+
+// Create an XML document
+$dom = new \DOMDocument();
+$dom->formatOutput = true;
+$root = $dom->createElement('hashes');
+$dom->appendChild( $root );
+foreach( $hashes as $file => $hash )
+{
+	$addHash( $root, $file, $hash );
+}
+
+// Sign the hashes document
+$certificate = file_get_contents( '... path to certificate file ...');
+$key = file_get_contents( '... path to private key file ...' );
+$signer = new \XBRL_Signer();
+$dom = $signer->sign_instance_dom( $dom, $key, $certificate );
+
+// Wite the signed content
+// file_put_contents( __DIR__ . '/sbr_hashes.xml', $dom->saveXML() );
+file_put_contents( 'd:/sbr_hashes.xml', $dom->saveXML() );
